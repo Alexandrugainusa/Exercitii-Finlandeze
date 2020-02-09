de5851e0ba1890a7984e2dbf66b3750763d68b77
Alex's Laptop <alexandruu.gainusa@yahoo.com>
2020-02-10 00:59:14 +0200

    JavaBuch13-Exercitii-Finlandeze

--- src/week4/exercise78/BoundedCounter.java
+++ src/week4/exercise78/BoundedCounter.java
@@ -1,4 +1,28 @@
 package week4.exercise78;
 
 public class BoundedCounter {
+    private int value = 0;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        this.upperLimit = upperLimit;
+    }
+
+    public void next() {
+        if (this.value == upperLimit){
+            this.value = 0;
+        }else{
+            this.value++;
+        }
+    }
+
+    public String toString() {
+        if (this.value<10){
+            return  ""+ "0"+this.value;
+        }
+        else {
+            return  ""+ this.value;
+        }
+
+    }
 }
