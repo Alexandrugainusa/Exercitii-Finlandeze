13516108c7064c84b22fbf4ab524ffe77ee6cf61
alexandrugainusa <alexandruu.gainusa@yahoo.com>
2022-05-14 18:12:52 +0300

    reformatory

--- src/week5/exercise85/Reformatory.java
+++ src/week5/exercise85/Reformatory.java
@@ -1,2 +1,20 @@
-package week5.exercise85;public class Reformatory {
+package week5.exercise85;
+
+import libs.Person;
+
+public class Reformatory {
+    private int numberOfWeightMeasured = 0;
+
+    public int weight(Person person){
+        this.numberOfWeightMeasured++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person){
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int totalWeightsMeasured(){
+        return this.numberOfWeightMeasured;
+    }
 }
